9683b7de7bc664a16a8d43f7367486793996c6e4
EmilHristov <emilovhr@gmail.com>
2023-11-09 15:57:03 -0500

    changes

--- src/components/products/ProductPage.js
+++ src/components/products/ProductPage.js
@@ -55,15 +55,15 @@ export default function ProductPage() {
                     <div>
                         <button
                             className="focus:outline-none text-white bg-green-700
-                        hover:bg-green-800 focus:ring-4 focus:ring-green-300
-                        font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2
-                        dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+                            hover:bg-green-800 focus:ring-4 focus:ring-green-300
+                            font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2
+                            dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
                             onClick={toggleMenu}
                         >
                             Toggle Nav Menu
                         </button>
                     </div>
-                    <div>Products & Shopping Cart</div>
+                    <div className="mt-[4px]">Products & Shopping Cart</div>
                     <div className="cart-logo-align">
                         <Link to="/cart">
                             <img
